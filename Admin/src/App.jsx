ca1d9cd8035469568fd5deb1b9e36c20c2086b59
chaithra <chaithradhanduguri@gmail.com>
2024-11-21 13:56:20 +0530

    created parcels and users page

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,62 +1,80 @@
-
-import {createBrowserRouter,Outlet,RouterProvider} from "react-router-dom";
-import Home from "./pages/Home"
-import Parcels from "./pages/Parcels"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import Home from "./pages/Home";
+import Parcels from "./pages/Parcels";
+import Users from "./pages/Users";
+import NewParcel from "./pages/NewParcel";
+import NewUsers from "./pages/NewUsers";
 import Parcel from "./pages/Parcel";
-import Navbar from "./components/Navbar"
-import Menu from "./components/Menu"
-import Footer from "./components/Footer"
-import Users from "./pages/Users"
-function App() {  
+import Login from "./pages/Login";
 
-  const Layout = () =>{
+function App() {
+  const Layout = () => {
+    return (
+      <div className="flex flex-col">
+        <Navbar />
 
-    return(
-      <div>
-        <Navbar/>
         <div className="flex">
           <div className="w-[20%]">
-            <Menu/>
+            <Menu />
           </div>
-          <div className="w-[80%]">
-            <Outlet/>
-          </div>
-          </div>
-            <Footer/>
-          </div>
-    )
-  }
 
-  const router = createBrowserRouter([ 
-    {path:"/",
-      element:<Layout/>,
-      children:[
-        {path:"/",
-          element:<Home/>
+          <div className="w-[80%]">
+            <Outlet />
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+
+        {
+          path: "/parcels",
+          element: <Parcels />,
         },
         
-        {path:"/parcels",
-          element:<Parcels/>
+        {
+          path: "/users",
+          element: <Users />,
         },
-        {path:"/parcel/:parcelId",
-          element:<Parcel/>
+        
+        {
+          path: "/newparcel",
+          element: <NewParcel />,
         },
-        {path:"/users",
-          element:<Users/>
+        {
+          path: "/newuser",
+          element: <NewUsers />,
         },
-      ]
+        {
+          path: "/parcel/:id",
+          element: <Parcel />,
+        },
+      ],
+     
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
 
-   
-
-  ])
-  return <>
-  <RouterProvider router={router}/>
-
-
-      
-    </>;
   
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
